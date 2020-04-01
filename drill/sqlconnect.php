7c8f4612548649c021f782d6323ab707bc16f319
pierre lorand <58699510+Pierre014@users.noreply.github.com>
2020-04-01 10:10:31 +0200

    Delete sqlconnect.php

--- drill/sqlconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    try{
-        $pdo = new PDO('mysql:host=localhost; dbname=becode','pierre','Feuille014');
-    }catch(Exception $e){
-        die('Erreur : '.$e->getMessage());
-    }
-?>
